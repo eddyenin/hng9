88f30794be086226914e58ff943dfbbaf435e0d9
Edidiong Daniel <eddyenin6@gmail.com>
2022-10-28 09:37:46 +0100

    Backend task cors fixed

--- routes/api.php
+++ routes/api.php
@@ -13,16 +13,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/eddyenin',function(){
-    $response = json_encode([
-        "slackUsername" => "eddyenin6",
-        "backend" => true,
-        "age" => 28,
-        "bio" => "My name is Edidiong Akpaenin. I am a web developer."
-    ]);
 
-    return $response;
-});
+Route::middleware(['cors'])->group(function () {
+    Route::get('/eddyenin',function(){
+        return response(["slackUsername" => "eddyenin6",
+            "backend" => true,
+            "age" => 28,
+            "bio" => "My name is Edidiong Akpaenin. I am a web developer."])
+            ->header('Content-type','application/json')
+            ->header('Access-Control-Allow-Origin', '*');
+    });
+ });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
